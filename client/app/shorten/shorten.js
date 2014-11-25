5035fa5abb2dc3b8c49903d2e44cf0705cde313c
aggfr12 <avidunn@gmail.com>
2014-11-24 23:46:33 -0800

    shorten passes specs

--- client/app/shorten/shorten.js
+++ client/app/shorten/shorten.js
@@ -2,7 +2,7 @@ angular.module('shortly.shorten', [])
 
 .controller('ShortenController', function ($scope, $location, Links) {
   angular.extend($scope, Links);
-  $scope.link = Links.holder;
+  $scope.link = {};
   $scope.addLink = function(mylink){
     Links.createNewLink(mylink)
     .then(function(data) {
@@ -13,6 +13,5 @@ angular.module('shortly.shorten', [])
       console.log("all unicorns die", err);
     });
   };
-  $scope.addLink();
 
 });
